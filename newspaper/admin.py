752a51166168e4b02202d289e7cb1c7486beab8c
Suman-bhujel1996 <suman.bhujel2468@gmail.com>
2024-05-29 07:27:53 +0545

    Add aznewspaper

---
+++ newspaper/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from newspaper.models import Category, Comment, Contact, Newsletter, Post, Tag, UserProfile
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Contact)
+admin.site.register(UserProfile)
+admin.site.register(Comment)
+admin.site.register(Newsletter)
+
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    date_hierarchy = "published_at"
+    
+
+admin.site.register(Post, PostAdmin)
